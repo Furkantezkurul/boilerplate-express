fdacc97e7d67af44a1ab01a4b9228e16071064dc
Furkan Tezkurul <ftezkuru@mail.uni-mannheim.de>
2023-11-19 18:58:52 +0100

    bug fixxed

--- myApp.js
+++ myApp.js
@@ -1,25 +1,29 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
-require('dotenv').config();
 
 console.log("Hello World");
 
 app.get("/", function (req, res) {
     //res.send("Hello Express");
     let path = __dirname + '/views/index.html';
+    console.log("test");
     res.sendFile(path);
 });
-
-let assetPath = __dirname + "/public";
-app.use("/public", express.static(assetPath));
 
 app.get("/json", function (req, res) {
     let responseMessage = "Hello json";
     if (process.env.MESSAGE_STYLE === "uppercase") {
         responseMessage = responseMessage.toUpperCase();
     }
-    res.json({"message": responseMessage });
+    
+    res.json({ message: responseMessage });
 });
+
+let assetPath = __dirname + "/public";
+app.use("/public", express.static(assetPath));
+
+
 
 
 
